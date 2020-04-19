403c26bbe3a23214dcbcff6def3f45d502ae4ef8
N. L <krisalovea@gmail.com>
2020-04-19 22:30:22 +0300

    Добавьте файлы проекта.

---
+++ Free__IP/Free_ip.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Free__IP
+{
+    partial class Scan
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Scann = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(274, 256);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Scann
+            // 
+            this.Scann.Location = new System.Drawing.Point(118, 312);
+            this.Scann.Name = "Scann";
+            this.Scann.Size = new System.Drawing.Size(58, 23);
+            this.Scann.TabIndex = 1;
+            this.Scann.Text = "Поиск";
+            this.Scann.UseVisualStyleBackColor = true;
+            this.Scann.Click += new System.EventHandler(this.Scann_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 286);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(58, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Подсеть";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(13, 274);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Занятые";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Scan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 347);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Scann);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Scan";
+            this.Text = "Free_IP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Scann;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Free__IP/Free_ip.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Free__IP
+{
+    public partial class Scan : Form
+    {
+        public Scan()
+        {
+            InitializeComponent();
+        }
+        int i;
+        string ip_S;
+        List<string> Lists = new List<string>();
+        List<string> List_IP = new List<string>();
+        private async void ip()
+        {
+            Invoke((MethodInvoker)async delegate
+            {
+                ip_S = textBox2.Text;
+                for (i = 0; i <= 254; i++)
+                {
+                    Lists.Add("192.168." + ip_S + "." + i); // добавляем наш ip в лист, для формирования списка
+                }
+                var reply = Lists.Select(async ip_L =>
+                {
+                    await Task.Delay(100);
+                    Ping ping = new Ping();
+                    byte[] packet = Encoding.ASCII.GetBytes("................................"); // наш минимальный пакет для отправки
+                    PingOptions packetOpt = new PingOptions(300, true);
+                    for (int b = 0; b <= 2; b++)
+                    {
+                        var results = await ping.SendPingAsync(ip_L, 500, packet, packetOpt);
+
+                        if (results.Status == IPStatus.Success)
+                        {
+                            if (checkBox1.Checked)
+                            {
+                                List_IP.Add("\r\n" + ip_L);
+                                List_IP = List_IP.GroupBy(x => x).Select(x => x.First()).ToList();
+                                textBox1.Text = "Занятые:";
+                                textBox1.Text = textBox1.Text + string.Join("", List_IP);
+                            }
+                        }
+                        else
+                        {
+                            if (!checkBox1.Checked)
+                            {
+                                List_IP.Add("\r\n" + ip_L);
+                                List_IP = List_IP.GroupBy(x => x).Select(x => x.First()).ToList();
+                                textBox1.Text = "Свободные:";
+                                textBox1.Text = textBox1.Text + string.Join("", List_IP);
+                            }
+                        }
+                    }
+                }).ToList();
+            });
+
+        }
+        private void Scann_Click(object sender, EventArgs e)
+        {
+            ip_S = "";
+            List_IP.Clear();
+            textBox1.Text = "";
+            Thread ip_Th = new Thread(ip);
+            ip_Th.Start();
+        }
+
+    }
+}
